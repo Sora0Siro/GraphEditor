4f6fe24eb5320192c7e7353bd9c57af91046d360
Sora0Siro <43938296+Sora0Siro@users.noreply.github.com>
2018-11-15 05:19:39 +0200

    *Few changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,29 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.StrokeType;
 
 public class Controller
 {
     private boolean deleteMode = false;
     private boolean addingMode = false;
+    private boolean addingLine = false;
+    private boolean addingArrow = false;
+
+    private Line line = new Line();
+
+    private boolean lineStarted = false;
+
+    //@FXML
+    //private Pane grandPane;
 
     @FXML
     private Pane myPane;
@@ -22,6 +34,12 @@ public class Controller
     @FXML
     private ImageView jointButtonId;
 
+    @FXML
+    private ImageView addLine;
+
+    @FXML
+    private ImageView addArrow;
+
     public void setJoint(MouseEvent event)
     {
         if(addingMode)
@@ -30,11 +48,27 @@ public class Controller
 
             circle.setCenterX(event.getX());
             circle.setCenterY(event.getY());
+            circle.setStroke(Color.BLACK);
+            circle.setStrokeWidth(2);
+            circle.setStrokeType(StrokeType.OUTSIDE);
             circle.setRadius(20.0f);
             circle.setFill(Color.GREEN);
 
             circle.setOnMouseClicked((MouseEvent event2) -> {
-                if(this.deleteMode) myPane.getChildren().remove(circle);
+                System.out.println("YOU CLICKED ON JOINT");
+                getAllNodes();
+                if(deleteMode)
+                {
+                    myPane.getChildren().remove(circle);
+                }
+                else if(this.lineStarted)
+                {
+                    setEndPositionFormJoint(circle);
+                }
+                else if(addingLine && !lineStarted)
+                {
+                    setStartPositionFromJoint(circle);
+                }
             });
 
             myPane.getChildren().add(circle);
@@ -46,6 +80,8 @@ public class Controller
         if(!addingMode)
         {
             if(deleteMode)setDeleteMode(event);
+            if(addingArrow)setArrowAdding(event);
+            if(addingLine)setLineAdding(event);
 
             this.addingMode = true;
             jointButtonId.setImage(new Image("/photo/circleAfterClickGreen.png"));
@@ -61,7 +97,9 @@ public class Controller
     {
         if(!deleteMode)
         {
-            if(addingMode)setAddingMode(event);
+            if(addingMode) setAddingMode(event);
+            if(addingArrow)setArrowAdding(event);
+            if(addingLine)setLineAdding(event);
 
             this.deleteMode = true;
             deleteModeId.setImage(new Image("/photo/circleAfterClickRed.png"));
@@ -71,4 +109,117 @@ public class Controller
             deleteModeId.setImage(new Image("/photo/add-button-inside-black-circle.png"));
         }
     }
+
+    public void setLineAdding(MouseEvent event)
+    {
+        if(!addingLine)
+        {
+            if(addingMode) setAddingMode(event);
+            if(addingArrow)setArrowAdding(event);
+            if(deleteMode) setDeleteMode(event);
+
+            this.addingLine = true;
+            addLine.setImage(new Image("/photo/lineAfterClick.png"));
+        }
+        else {
+            this.addingLine = false;
+            addLine.setImage(new Image("/photo/substract.png"));
+        }
+    }
+
+    public void setArrowAdding(MouseEvent event)
+    {
+        if(!addingArrow)
+        {
+            if(addingMode) setAddingMode(event);
+            if(addingLine)setLineAdding(event);
+            if(deleteMode) setDeleteMode(event);
+
+            this.addingArrow = true;
+            addArrow.setImage(new Image("/photo/arrowAfterClick.png"));
+        }
+        else {
+            this.addingArrow = false;
+            addArrow.setImage(new Image("/photo/left-arrow.png"));
+        }
+    }
+
+    //line from joint
+    public void setStartPositionFromJoint(Circle circle)
+    {
+        line = new Line();
+        myPane.getChildren().add(line);
+
+        line.setStartX(circle.getCenterX());
+        line.setStartY(circle.getCenterY());
+
+        this.lineStarted = true;
+    }
+
+    public void whileMouseMoving(MouseEvent event)
+    {
+        if(this.lineStarted)
+        {
+            line.setEndX(event.getX()-5);
+            line.setEndY(event.getY()-5);
+            line.setVisible(true);
+        }
+    }
+
+    public void setEndPositionFormJoint(Circle circle)
+    {
+        if(this.lineStarted)
+        {
+            line.setEndX(circle.getCenterX());
+            line.setEndY(circle.getCenterY());
+
+            this.lineStarted = false;
+        }
+    }
+
+    public void getAllNodes()
+    {
+        for(Node n:myPane.getChildren())
+        {
+            //Circle[centerX=362.0, centerY=367.0, radius=20.0, fill=0x008000ff, stroke=0x000000ff, strokeWidth=2.0]
+            //Line[startX=362.0, startY=367.0, endX=233.0, endY=189.0, stroke=0x000000ff, strokeWidth=1.0]
+        }
+    }
+
+
+    /*public void setStartPosition(MouseEvent event)
+    {
+        if(addingLine)
+        {
+            line = new Line();
+            line.setVisible(false);
+            line.setStartX(event.getX());
+            line.setStartY(event.getY());
+
+            myPane.getChildren().add(line);
+        }
+    }*/
+
+    /*public void refreshWhileDragged(MouseEvent event)
+    {
+        if(addingLine && (line.getEndX()!= 0))
+        {
+            line.setEndX(event.getX());
+            line.setEndY(event.getY());
+            line.setVisible(true);
+        }
+    }*/
+
+    /*public void setEndPosition(MouseEvent event)
+    {
+        if(addingLine)
+        {
+            System.out.println("END : ");
+            System.out.println("END X: " + String.valueOf(event.getX()));
+            System.out.println("END Y: " + String.valueOf(event.getX()));
+
+            line.setEndX(event.getX());
+            line.setEndY(event.getY());
+        }
+    }*/
 }
